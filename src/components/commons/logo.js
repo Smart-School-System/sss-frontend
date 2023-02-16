95a5e237857b31efbeb009be91b4dfb0e5f0a9e1
codingdrizzle <codingfrancis100@gmail.com>
2023-02-16 22:21:12 +0000

    made some changes

--- src/components/commons/logo.js
+++ src/components/commons/logo.js
@@ -9,7 +9,6 @@ const Logo = () => {
 		<div className='flex justify-between items-center space-x-2 cursor-pointer' onClick={() => navigate('/')}>
 			<img src={logo} alt="app-logo"/>
 			<span className='font-black text-xl text-darkFontColor dark:text-darkThemeTextLight'>Schoolar</span>
-            
 		</div>
 	);
 };
