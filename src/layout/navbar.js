6162f3c350508f86b8f6242b8cd93cc78a35cae4
codingdrizzle <codingfrancis100@gmail.com>
2023-02-16 22:23:36 +0000

    made some changes

--- src/layout/navbar.js
+++ src/layout/navbar.js
@@ -10,16 +10,16 @@ const Navbar = ({ leftValue }) => {
 	const [isPoped, setPoped] = useState(false);
 	return (
 		<header className='navbar fixed left-40 right-0 top-0 z-30 bg-white dark:bg-darkTheme4 flex items-center space-x-20 px-12 h-32' style={{ left: leftValue }}>
-			<nav className="flex justify-between align-middle h-auto w-full">
+			<nav className='flex justify-between align-middle h-auto w-full'>
 				<Logo />
-				<div className="flex justify-end items-center space-x-6">
+				<div className='flex justify-end items-center space-x-6'>
 					<Link to={''}>
 						<FaBell size={25} className='text-greyFontColor dark:text-darkThemeTextLight' />
 					</Link>
 					<Toggle/>
 					<div onMouseOver={() => setPoped(true)} onMouseOut={() => setPoped(false)} className='cursor-pointer'>
 						<div>
-							<img src={user} alt="" className='rounded-full'/>
+							<img src={user} alt='' className='rounded-full'/>
 						</div>
 						<div style={{ borderWidth: 1 }} className={`${!isPoped && 'hidden'} flex-col space-y-3 bg-white dark:bg-darkTheme1 border-transparent dark:border-darkTheme3 absolute top-23 right-2 drop-shadow-md w-auto h-auto p-2 rounded-lg`}>
 							<Link to={''} className='flex justify-start p-2 px-5 items-center space-x-4 text-greyFontColor dark:text-darkThemeTextLight dark:hover:bg-darkTheme3 hover:text-primaryBlue hover:bg-lightBlue rounded-lg'><FaUserAlt /><span>Profile</span></Link>
