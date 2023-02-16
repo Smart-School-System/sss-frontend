eb52aa97102a0743f08499ff19e28415306710d2
codingdrizzle <codingfrancis100@gmail.com>
2023-02-16 22:21:23 +0000

    made some changes

--- src/components/commons/logo.js
+++ src/components/commons/logo.js
@@ -5,7 +5,6 @@ import logo from '../../assets/logo/logo.svg';
 const Logo = () => {
 	const navigate = useNavigate();
 	return (
-        
 		<div className='flex justify-between items-center space-x-2 cursor-pointer' onClick={() => navigate('/')}>
 			<img src={logo} alt="app-logo"/>
 			<span className='font-black text-xl text-darkFontColor dark:text-darkThemeTextLight'>Schoolar</span>
