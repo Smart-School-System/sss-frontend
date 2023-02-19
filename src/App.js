e312846db012d9e43fea733d6e309ed8d01ff164
codingdrizzle <codingfrancis100@gmail.com>
2023-02-19 19:11:32 +0000

    working on the login page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { HashRouter } from 'react-router-dom';
 import AppRoutes from './router';
-// import 'antd/dist/reset.css';
 import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
 
 function App() {
 	return (
